9e63693bd0a2b38c955747d1cb689d70829c0b90
Aram-Vn <aramvardanyan13@gmail.com>
2024-01-08 19:09:25 +0400

    trying to add f_itr

--- src/Single_linked_list.h
+++ src/Single_linked_list.h
@@ -1,68 +1,97 @@
 #ifndef SINGLE_LINKED_LIST_H
 #define SINGLE_LINKED_LIST_H
 
+#include <iterator>
 namespace my {
-	template <class T>
-	class Forward_list
-	{
-	public:
-		//destructor
-		~Forward_list(); // Destructor to deallocate memory used by the linked list nodes
+    template <class T>
+    class Forward_list
+    {
+    private:
+        struct Node
+        {
+        public:
+            Node(T val, Node* node_ptr); // Node parameterized constructor
+            ~Node() = default;
 
-		//Constructor
-		Forward_list(); //  Default constructor to create an empty linked list, m_head = nullprt
-		Forward_list(const Forward_list& other); // Copy constructor to create a new list as a copy of another list
-		Forward_list(Forward_list&& other) noexcept; //	Move constructor to efficiently transfer ownership of resources
-		Forward_list(std::initializer_list<T> init_list); // Constructor to initialize the list with elements from an initializer list
-	
-		//Opreators
-		Forward_list& operator=(const Forward_list& other); // Copy assignment operator to assign the contents of another list
-		Forward_list& operator=(Forward_list&& other) noexcept; // Move assignment operator to efficiently transfer ownership of resources	
+        public:
+            T m_val;
+            Node* m_next = nullptr;
+        };
 
-	public:
-		//Element access
-		T front() const; // Retrieve the value of the first element
-		T back() const; // Retrieve the value of the last element
-		void print(); // Print the elements of the list
-	
-		//Capacity
-		bool empty() const; // Check if the list is empty, if empty returnde true, else false 
-		size_t size() const; // Get the number of elements in the list
+    public:
+        class Forword_itr
+        {
+        public:
+            Forword_itr();
+            Forword_itr(Node* ptr);
+            Forword_itr& operator=(const Forword_itr& other);
+            Node& operator*();
+            Node* operator->();
+            Forword_itr& operator++();
+            Forword_itr operator++(int);
+            bool operator==(const Forword_itr& other);
+            bool operator!=(const Forword_itr& other);
 
-		//Modifiers
-		void clear(); // Remove all elements from the list, m_head = nullptr;
-		void insert(int index, const T& value); // Insert an element ( value ) at the specified index 
-		void erase(int index); //  Erase the element at the specified index (if index >= 0 && < size())
-		void push_back(int data); // Add an element to the end of the list
-		void pop_back(); // Remove the last element from the list
-		void push_front(int data); // Add an element to the beginning of the list
-		void pop_front(); // Remove the first element from the list
-		void swap(Forward_list& other); // Swap the contents of two lists
-		void assign(int count, const T& value); // removes all prevues value, Assign a specified value to a specified number of elements, 
-		void assign(const std::initializer_list<T>& init_list); //  removes all prevues value, Assign elements from an initializer list
+        private:
+            Node* ptr;
+        };
 
-		//Operations
-		void reverse(); // Reverse the order of elements in the list
-		void insertionSort(); // surtes list by using insertion Sort algorithm
-		/* Forward_list<T>& merge(Forward_list<T>& other); */	
+    public:
+        using f_itr = Forword_itr;
+        f_itr begin();
+        f_itr end();
 
-	private:
-		struct Node
-		{
-		public:
-			Node(T val, Node* node_ptr); // Node parameterized constructor
-			~Node() = default; 			
+    public:
+        // destructor
+        ~Forward_list(); // Destructor to deallocate memory used by the linked list nodes
 
-		public:
-			T m_val;
-			Node* m_next = nullptr;		
-		};
-		Node* m_head;
+        // Constructor
+        Forward_list(); //  Default constructor to create an empty linked list, m_head = nullprt
+        Forward_list(const Forward_list& other);     // Copy constructor to create a new list as a copy of another list
+        Forward_list(Forward_list&& other) noexcept; //	Move constructor to efficiently transfer ownership of resources
+        Forward_list(std::initializer_list<T> init_list); // Constructor to initialize the list with elements from an
+                                                          // initializer list
 
-	};
+        // Opreators
+        Forward_list& operator=(const Forward_list& other);     // Copy assignment operator to assign the contents of
+                                                                // another list
+        Forward_list& operator=(Forward_list&& other) noexcept; // Move assignment operator to efficiently transfer
+                                                                // ownership of resources
 
-	#include "Single_linked_list_impl.hpp"
-}
+    public:
+        // Element access
+        T front() const; // Retrieve the value of the first element
+        T back() const;  // Retrieve the value of the last element
+        void print();    // Print the elements of the list
 
-		
-#endif //SINGLE_LINKED_LIST_H
+        // Capacity
+        bool empty() const;  // Check if the list is empty, if empty returnde true, else false
+        size_t size() const; // Get the number of elements in the list
+
+        // Modifiers
+        void clear();                           // Remove all elements from the list, m_head = nullptr;
+        void insert(int index, const T& value); // Insert an element ( value ) at the specified index
+        void erase(int index);                  //  Erase the element at the specified index (if index >= 0 && < size())
+        void push_back(int data);               // Add an element to the end of the list
+        void pop_back();                        // Remove the last element from the list
+        void push_front(int data);              // Add an element to the beginning of the list
+        void pop_front();                       // Remove the first element from the list
+        void swap(Forward_list& other);         // Swap the contents of two lists
+        void assign(int count, const T& value); // removes all prevues value, Assign a specified value to a specified
+                                                // number of elements,
+        void assign(const std::initializer_list<T>& init_list); //  removes all prevues value, Assign elements from an
+                                                                //  initializer list
+
+        // Operations
+        void reverse();       // Reverse the order of elements in the list
+        void insertionSort(); // surtes list by using insertion Sort algorithm
+                              /* Forward_list<T>& merge(Forward_list<T>& other); */
+
+    private:
+        Node* m_head;
+    };
+
+#include "Single_linked_list_impl.hpp"
+} // namespace my
+
+#endif // SINGLE_LINKED_LIST_H
--- src/Single_linked_list_impl.hpp
+++ src/Single_linked_list_impl.hpp
@@ -1,79 +1,89 @@
 #ifndef SINGLE_LINKED_LIST_IMPL_HPP
 #define SINGLE_LINKED_LIST_IMPL_HPP
 
+//-----------------------forword_itr_-----------------------------//
+template <class T>
+my::Forward_list<T>::Forward_itr::Forward_itr() : ptr(nullptr)
+{
+}
 
 template <class T>
-my::Forward_list<T>::Forward_list() :
-	m_head(nullptr)
-{}
+my::Forward_list<T>::Forward_list() : m_head(nullptr)
+{
+}
 template <class T>
 my::Forward_list<T>::~Forward_list()
 {
-	while (m_head) {
-		Node* temp = m_head;
-		m_head = m_head->m_next;
-		delete temp;
-	}
+    while (m_head)
+    {
+        Node* temp = m_head;
+        m_head = m_head->m_next;
+        delete temp;
+    }
 }
 
 template <typename T>
 my::Forward_list<T>::Forward_list(const Forward_list& other)
 {
-    if (!other.m_head) {
-    	return;
-	}
+    if (!other.m_head)
+    {
+        return;
+    }
 
     Node* current = other.m_head;
     this->m_head = new Node(current->m_val, nullptr);
     Node* this_current = this->m_head;
-	current = current->m_next;
+    current = current->m_next;
 
-   	while (current) {
+    while (current)
+    {
         this_current->m_next = new Node(current->m_val, nullptr);
         current = current->m_next;
-    	this_current = this_current->m_next;
-	}
-}
-
-template <class T>
-my::Forward_list<T>::Forward_list(Forward_list&& other) noexcept :
-	m_head{other.m_head}
-{
-	other.m_head = nullptr;
-}
-
-template <class T> 
-my::Forward_list<T>::Node::Node(T val, Node* node_ptr) :
-	m_val{val},
-	m_next{node_ptr}
-{}
-
-template <class T> 
-my::Forward_list<T>::Forward_list(std::initializer_list<T> init_list) :
-	m_head(nullptr) 
-{
-	for (const auto& elem : init_list) {
-		this->push_back(elem);
+        this_current = this_current->m_next;
     }
 }
 
+template <class T>
+my::Forward_list<T>::Forward_list(Forward_list&& other) noexcept : m_head{ other.m_head }
+{
+    other.m_head = nullptr;
+}
 
-template <class T> 
+template <class T>
+my::Forward_list<T>::Node::Node(T val, Node* node_ptr) : m_val{ val }, m_next{ node_ptr }
+{
+}
+
+template <class T>
+my::Forward_list<T>::Forward_list(std::initializer_list<T> init_list) : m_head(nullptr)
+{
+    for (const auto& elem : init_list)
+    {
+        this->push_back(elem);
+    }
+}
+
+template <class T>
 my::Forward_list<T>& my::Forward_list<T>::operator=(const Forward_list& other)
-{	
-	if (this != &other) {
-    	clear();
+{
+    if (this != &other)
+    {
+        clear();
 
         Node* otherCurrent = other.m_head;
         Node* newCurrent = nullptr;
 
-        while (otherCurrent) {
+        while (otherCurrent)
+        {
             Node* newNode = new Node(otherCurrent->m_val, nullptr);
 
-            if (!m_head) {
+            if (!m_head)
+            {
                 m_head = newNode;
                 newCurrent = m_head;
-            } else {
+            }
+            else
+            {
                 newCurrent->m_next = newNode;
                 newCurrent = newCurrent->m_next;
             }
@@ -85,88 +95,98 @@ my::Forward_list<T>& my::Forward_list<T>::operator=(const Forward_list& other)
     return *this;
 }
 
-template <class T> 
+template <class T>
 my::Forward_list<T>& my::Forward_list<T>::operator=(Forward_list&& other) noexcept
 {
-	if (this != &other) { 
+    if (this != &other)
+    {
         clear();
 
         m_head = other.m_head;
         other.m_head = nullptr;
     }
 
-	return *this;
+    return *this;
 }
 
 template <class T>
 void my::Forward_list<T>::push_front(int data)
 {
-	Node* new_node = new Node{data, m_head};
+    Node* new_node = new Node{ data, m_head };
     m_head = new_node;
 }
 
 template <class T>
-void my::Forward_list<T>::push_back(int data) 
+void my::Forward_list<T>::push_back(int data)
 {
-  	if (!m_head) {
-        m_head = new Node{data, nullptr};
-    	return;
+    if (!m_head)
+    {
+        m_head = new Node{ data, nullptr };
+        return;
     }
 
     Node* current = m_head;
 
-	while (current->m_next) {
+    while (current->m_next)
+    {
         current = current->m_next;
     }
 
-    current->m_next = new Node{data, nullptr};
+    current->m_next = new Node{ data, nullptr };
 }
 
-template <class T>   
-void my::Forward_list<T>::pop_front() 
+template <class T>
+void my::Forward_list<T>::pop_front()
 {
-	if (!m_head) {
-    	std::cout << "List is empty!!!\nin pop_front()" << std::endl;
-   		exit(0);
-	 }
+    if (!m_head)
+    {
+        std::cout << "List is empty!!!\nin pop_front()" << std::endl;
+        exit(0);
+    }
 
     Node* tmp_ptr = m_head;
     m_head = m_head->m_next;
     delete tmp_ptr;
 }
 
-template <class T>  
-void my::Forward_list<T>::pop_back() 
+template <class T>
+void my::Forward_list<T>::pop_back()
 {
-	if (!m_head) {
-    	std::cout << "List is empty!!!\nCannot pop_back()." << std::endl;
-		exit(0);
-	}
+    if (!m_head)
+    {
+        std::cout << "List is empty!!!\nCannot pop_back()." << std::endl;
+        exit(0);
+    }
 
-	if (!m_head->m_next) {
-    	delete m_head;
-   		m_head = nullptr;
-    	return;
-	}
+    if (!m_head->m_next)
+    {
+        delete m_head;
+        m_head = nullptr;
+        return;
+    }
 
-	Node* current = m_head;
+    Node* current = m_head;
 
-	while (current->m_next->m_next) {
-   		current = current->m_next;
-	}
+    while (current->m_next->m_next)
+    {
+        current = current->m_next;
+    }
 
-	delete current->m_next;
-	current->m_next = nullptr;
+    delete current->m_next;
+    current->m_next = nullptr;
 }
 
 template <class T>
-void my::Forward_list<T>::insert(int pos, const T& value) {
-    if (pos < 0) {
+void my::Forward_list<T>::insert(int pos, const T& value)
+{
+    if (pos < 0)
+    {
         std::cout << "In insert\nFirst argument must be >= 0" << std::endl;
         exit(0);
     }
 
-    if (pos == 0) {
+    if (pos == 0)
+    {
         Node* new_node = new Node(value, m_head);
         m_head = new_node;
         return;
@@ -175,29 +195,37 @@ void my::Forward_list<T>::insert(int pos, const T& value) {
     int count = 0;
     Node* current = m_head;
 
-    while (current->m_next && count < pos - 1) {
+    while (current->m_next && count < pos - 1)
+    {
         current = current->m_next;
         ++count;
     }
 
-    if (count == pos - 1) {
+    if (count == pos - 1)
+    {
         Node* new_node = new Node(value, current->m_next);
         current->m_next = new_node;
-    } else {
+    }
+    else
+    {
         std::cout << "In insert\nThere is no such index" << std::endl;
         exit(0);
     }
 }
 
 template <class T>
-void my::Forward_list<T>::erase(int index) {
-    if (index < 0) {
+void my::Forward_list<T>::erase(int index)
+{
+    if (index < 0)
+    {
         std::cout << "In erase\nIndex must be >= 0" << std::endl;
         exit(0);
     }
 
-    if (index == 0) {
-        if (m_head) {
+    if (index == 0)
+    {
+        if (m_head)
+        {
             Node* temp = m_head;
             m_head = m_head->m_next;
             delete temp;
@@ -209,138 +237,156 @@ void my::Forward_list<T>::erase(int index) {
     Node* current = m_head;
     Node* previous = nullptr;
 
-    while (current && count < index) {
+    while (current && count < index)
+    {
         previous = current;
         current = current->m_next;
         ++count;
     }
 
-    if (count == index && current) {
-        if (previous) {
+    if (count == index && current)
+    {
+        if (previous)
+        {
             previous->m_next = current->m_next;
             delete current;
         }
-    } else {
+    }
+    else
+    {
         std::cout << "In erase\nIndex is out of bounds" << std::endl;
-		exit(0);
-	}
-}
-
-template <class T> 
-T my::Forward_list<T>::front() const
-{
-	if (!m_head) {
-		std::cout << "List is empty!!!\nCannot front()." << std::endl;
-		exit(0);
-	}
-
-	return m_head->m_val;
-}
-
-template <class T> 
-T my::Forward_list<T>::back() const
-{
-	if (!m_head) {
-		std::cout << "List is empty!!!\nCannot back()." << std::endl;
-		exit(0);
-	}
-	
-	Node* current = m_head;
-
-	if (!m_head->m_next) {
-		return m_head->m_val;
-	}
-
-	while (current->m_next->m_next) {
-   		current = current->m_next;
-	}
-
-	return current->m_next->m_val;
-}
-
-template <class T> 
-bool my::Forward_list<T>::empty() const
-{
-	return !(m_head); 
-}
-
-template <class T> 
-size_t my::Forward_list<T>::size() const
-{
-	if (!m_head) {
-		return 0;
-	}
-
-	size_t size = 1;
-	
-	Node* tmp_ptr = m_head;
-	while (tmp_ptr->m_next) {
-		tmp_ptr = tmp_ptr->m_next;
-		++size;
-	}
-
-	return size;
-}
-
-template <class T> 
-void my::Forward_list<T>::clear()
-{
-	while (m_head) {
-		Node* temp = m_head;
-		m_head = m_head->m_next;
-		delete temp;
-	}
-	m_head = nullptr;
-}
-
-template <class T> 
-void my::Forward_list<T>::swap(Forward_list& other)
-{
-	std::swap(this->m_head, other.m_head);	
-}
-
-template <class T> 
-void my::Forward_list<T>::assign(int count, const T& value)
-{
-	if (count < 0) {
-		std::cout << "in void assign(int count, const T& value)" << std::endl;
-		std::cout << "count must be >= 0" << std::endl;
-		exit(0);
-	}
-
-	this->clear();
-	
-	for	(int i = 0; i < count; ++i) {
-		this->push_back(value);
-	} 
-}
-
-template <class T>   
-void my::Forward_list<T>::assign(const std::initializer_list<T>& init_list)
-{
-	this->clear(); 
-
-	for (const T& elem : init_list) {
-		this->push_back(elem);
+        exit(0);
     }
 }
 
-template <class T> 
-void my::Forward_list<T>::print()
+template <class T>
+T my::Forward_list<T>::front() const
 {
-	Node* current = m_head;
+    if (!m_head)
+    {
+        std::cout << "List is empty!!!\nCannot front()." << std::endl;
+        exit(0);
+    }
 
-	while (current->m_next) {
-		std::cout << current->m_val << " ";
-        current = current->m_next;
-   	}
-	std::cout << current->m_val << " ";
-	std::cout << std::endl;
+    return m_head->m_val;
 }
 
 template <class T>
-void my::Forward_list<T>::reverse() {
-    if (!m_head || !m_head->m_next) {
+T my::Forward_list<T>::back() const
+{
+    if (!m_head)
+    {
+        std::cout << "List is empty!!!\nCannot back()." << std::endl;
+        exit(0);
+    }
+
+    Node* current = m_head;
+
+    if (!m_head->m_next)
+    {
+        return m_head->m_val;
+    }
+
+    while (current->m_next->m_next)
+    {
+        current = current->m_next;
+    }
+
+    return current->m_next->m_val;
+}
+
+template <class T>
+bool my::Forward_list<T>::empty() const
+{
+    return !(m_head);
+}
+
+template <class T>
+size_t my::Forward_list<T>::size() const
+{
+    if (!m_head)
+    {
+        return 0;
+    }
+
+    size_t size = 1;
+
+    Node* tmp_ptr = m_head;
+    while (tmp_ptr->m_next)
+    {
+        tmp_ptr = tmp_ptr->m_next;
+        ++size;
+    }
+
+    return size;
+}
+
+template <class T>
+void my::Forward_list<T>::clear()
+{
+    while (m_head)
+    {
+        Node* temp = m_head;
+        m_head = m_head->m_next;
+        delete temp;
+    }
+    m_head = nullptr;
+}
+
+template <class T>
+void my::Forward_list<T>::swap(Forward_list& other)
+{
+    std::swap(this->m_head, other.m_head);
+}
+
+template <class T>
+void my::Forward_list<T>::assign(int count, const T& value)
+{
+    if (count < 0)
+    {
+        std::cout << "in void assign(int count, const T& value)" << std::endl;
+        std::cout << "count must be >= 0" << std::endl;
+        exit(0);
+    }
+
+    this->clear();
+
+    for (int i = 0; i < count; ++i)
+    {
+        this->push_back(value);
+    }
+}
+
+template <class T>
+void my::Forward_list<T>::assign(const std::initializer_list<T>& init_list)
+{
+    this->clear();
+
+    for (const T& elem : init_list)
+    {
+        this->push_back(elem);
+    }
+}
+
+template <class T>
+void my::Forward_list<T>::print()
+{
+    Node* current = m_head;
+
+    while (current->m_next)
+    {
+        std::cout << current->m_val << " ";
+        current = current->m_next;
+    }
+    std::cout << current->m_val << " ";
+    std::cout << std::endl;
+}
+
+template <class T>
+void my::Forward_list<T>::reverse()
+{
+    if (!m_head || !m_head->m_next)
+    {
         return;
     }
 
@@ -348,7 +394,8 @@ void my::Forward_list<T>::reverse() {
     Node* current = m_head;
     Node* next = nullptr;
 
-    while (current) {
+    while (current)
+    {
         next = current->m_next;
         current->m_next = prev;
         prev = current;
@@ -358,26 +405,32 @@ void my::Forward_list<T>::reverse() {
     m_head = prev;
 }
 
-template <class T> 
+template <class T>
 void my::Forward_list<T>::insertionSort()
-{              
-    if (!m_head || !m_head->m_next) { 
-        return; 
+{
+    if (!m_head || !m_head->m_next)
+    {
+        return;
     }
- 
+
     Node* sorted = nullptr;
     Node* current = m_head;
 
-    while (current) {          
+    while (current)
+    {
         Node* next = current->m_next;
 
-        if (!sorted || current->m_val < sorted->m_val) {
+        if (!sorted || current->m_val < sorted->m_val)
+        {
             current->m_next = sorted;
-            sorted = current;  
-        } else {  
-            Node* temp = sorted;                                          
+            sorted = current;
+        }
+        else
+        {
+            Node* temp = sorted;
 
-            while (temp->m_next && current->m_val > temp->m_next->m_val) { 
+            while (temp->m_next && current->m_val > temp->m_next->m_val)
+            {
                 temp = temp->m_next;
             }
 
@@ -435,5 +488,6 @@ void my::Forward_list<T>::insertionSort()
 /* 	other.size = 0; */
 /* 	return (*this); */
 /* } */
-  
+
+
 #endif // SINGLE_LINKED_LIST_H
